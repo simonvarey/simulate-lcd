75a3792b3cdbf8e000a04ade8c0f4ea24926ff9f
simonvarey <simonvarey@gmail.com>
2023-01-24 23:25:46 -0500

    FEAT: added dev test

--- src/lib.rs
+++ src/lib.rs
@@ -195,9 +195,33 @@ impl<const R: usize, const C: usize> LcdScreen<R, C> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use sdl2::{event::Event, keyboard::Keycode};
+
+    const DARK: Color = Color::RGB(69, 75, 59);
+    const GREEN: Color = Color::RGB(158, 171, 136);
 
     #[test]
-    fn test() {
-
+    fn development_test() {
+        let sdl_context = sdl2::init().unwrap();
+        let mut screen = LcdScreen::<50, 50>::new(&sdl_context, "LCD Test", 
+        DARK, GREEN, 10, 10).unwrap();
+      
+        let bm: Box<Bitmap<50, 50>> = vec![[false; 50]; 50].try_into().unwrap();
+      
+        let mut event_pump = sdl_context.event_pump().unwrap();
+        'running: loop {
+          for event in event_pump.poll_iter() {
+            match event {
+              // Quit
+              Event::Quit {..} |
+              Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                break 'running
+              },
+              _ => { }
+            }
+          }
+      
+          screen.draw_bitmap(bm.as_ref()).unwrap();
+        }
     }
 }
