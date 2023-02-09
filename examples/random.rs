c95d585e5836469009a7b715d31d0c255cac562b
simonvarey <simonvarey@gmail.com>
2023-02-08 22:37:17 -0500

    EXAMPLE: Clean-up Random

--- examples/random.rs
+++ examples/random.rs
@@ -3,21 +3,8 @@ use std::{thread::sleep, time::Duration};
 
 use rand::{thread_rng, Rng};
 use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
-use simulate_lcd::{Bitmap, LcdScreen};
 
-fn random_bitmap<const C: usize, const R: usize>() -> Bitmap<C, R> {
-    let mut rng = thread_rng();
-
-    let bits_vec: Vec<[bool; C]> = (0..R as i32)
-        .map(|_y| {
-            let mut row = [false; C];
-            rng.fill(&mut row);
-            row
-        })
-        .collect();
-
-    bits_vec.try_into().unwrap()
-}
+use simulate_lcd::LcdScreen;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -35,7 +22,6 @@ fn main() {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                // Quit
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
@@ -45,7 +31,9 @@ fn main() {
             }
         }
 
-        screen.draw_bitmap(&random_bitmap()).unwrap();
+        let mut rng = thread_rng();
+        let random_bits: Vec<[bool; 50]> = (0..15).map(|_| rng.gen()).collect();
+        screen.draw_bitmap(&random_bits.try_into().unwrap()).unwrap();
 
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
